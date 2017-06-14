Jakeable/rforms

commit eefd5646d88db5174731aa11a5c08dfaab6742c6

fixed boolean setting issue
thanks to u/reseph for finding this issue!

static/modals.js
@@ -50,7 +50,7 @@ function editSetting(name, title, help) {
         text: "Submit",
         btnClass: "btn-blue",
         action: function() {
-          var inp = this.$content.find(".input").val();
+          var inp = $("input[name='input-answer']:checked").val();
           if (!inp) {
             $.alert("Please provide a valid response.");
             return false;

commit c8f4018f13b54d8b0abf61bc215b33ab75d22601

fixed setting input issue
this fixes an issue caused by eefd5646d88db5174731aa11a5c08dfaab6742c6, which didn't account for string or integer inputs.

static/modals.js
@@ -50,11 +50,20 @@ function editSetting(name, title, help) {
         text: "Submit",
         btnClass: "btn-blue",
         action: function() {
-          var inp = $("input[name='input-answer']:checked").val();
+          if (setting.type == "bool") {
+            var inp = $("input[name='input-answer']:checked").val();
+          }
+          else {
+            var inp = this.$content.find(".input").val();
+          }
+          
           if (!inp) {
             $.alert("Please provide a valid response.");
             return false;
           } else {
+            if (setting.type == "int") {
+              inp = Number(inp);
+            }
             $.post("/api/update_setting", {
               setting: name,
               data: inp